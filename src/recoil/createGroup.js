4bbee394c34b4b0a5eb9d6e476b5918eeaf70dda
Jalastin <justin_rusit@yahoo.com>
2021-12-16 14:46:49 -0800

    createGroup work part 2

--- src/recoil/createGroup.js
+++ src/recoil/createGroup.js
@@ -5,55 +5,44 @@ import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import { db } from "./db.js";
 import { userStateAtom } from "./atoms/userAtom.js";
 import { groupStateAtom } from "./atoms/groupAtom";
-import { SetUserGroup } from "./selectors/selectors.js"; 
+import { SetUserGroupID, SetUserPendingGroups, SetUserPendingInvitations } from "./selectors/selectors.js"; 
 
 function CreateGroupButton({}) {
     const [user, setUser] = useRecoilState(userStateAtom);
     const [group, setGroup] = useRecoilState(groupStateAtom);
-    const setUserGroup = useSetRecoilState(SetUserGroup);
+    const setUserGroupID = useSetRecoilState(SetUserGroupID);
+    const setUserPendingGroups = useSetRecoilState(SetUserPendingGroups);
+    const setUserPendingInvitations = useSetRecoilState(SetUserPendingInvitations);
 
-    const newGroupID = "newgrouptest";
+    const newGroupID = "newgrouptest4";
 
     async function createGroup() {
         // first, create a group locally
-        // and update the group atom
-
-        // get the current group from firebase just as a template
-        const docRef = doc(db, "2022-groups", "C5VaLwp0TjZCj2erPcaF");
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-            // actually create a new group now
-            var newGroup = docSnap.data();
-            newGroup.contact_email = user.email;
-            newGroup.description = "New Group!";
-            newGroup.group_id = newGroupID;
-            newGroup.members.some_users_uuid[0] = user.user_id;
-            newGroup.pending_members = [];
-            setGroup(newGroup);
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-        }
-
-        // add this group to the firebase
-        await setDoc(doc(db, "2022-groups", newGroupID), {
+        var newGroup = {
             contact_email: user.email,
-            description: "New Group!",
+            description: "New Group Test Again!",
             group_id: newGroupID,
-            max_members: 4,
             members: [user.user_id],
             pending_members: [],
             tags1: [],
             tags2: [],
-        });
+        };
 
-        // update the current user's group
+        // add this group to the firebase
+        await setDoc(doc(db, "2022-groups", newGroupID), newGroup);
+
+        // update the current user's group on firebase
         const docRef2 = doc(db, "2022-users", user.user_id);
         await updateDoc(docRef2, {
-            group_id: newGroupID
+            group_id: newGroupID,
+            pending_groups: [],
         });
+
         // setRSVP for front-end Recoil atom
-        setUserGroup(group.group_id); 
+        setGroup(newGroup);
+        setUserGroupID(newGroupID);
+        setUserPendingGroups([]);
+        setUserPendingInvitations([]);
     }
     
     return (
--- src/recoil/selectors/selectors.js
+++ src/recoil/selectors/selectors.js
@@ -14,15 +14,29 @@ export const SetRSVPState = selector({
 
 });
 
-export const SetUserGroup = selector({
-    // Note: with the new method that Omid added,
-    // having the key for the selector is no longer necessary(?)
-    // key: 'rsvp_status', // unique ID (with respect to other atoms/selectors)
+export const SetUserGroupID = selector({
     get: ({get}) => ({...get(userStateAtom)}), 
     set: ({set, get}, new_group_id) => { 
         let userStateCopy = {...get(userStateAtom)}
         userStateCopy.group_id = new_group_id; 
         set(userStateAtom, userStateCopy)
     }, 
+});
 
+export const SetUserPendingGroups = selector({
+    get: ({get}) => ({...get(userStateAtom)}), 
+    set: ({set, get}, pending_groups) => { 
+        let userStateCopy = {...get(userStateAtom)}
+        userStateCopy.pending_groups = pending_groups; 
+        set(userStateAtom, userStateCopy)
+    }, 
+});
+
+export const SetUserPendingInvitations = selector({
+    get: ({get}) => ({...get(userStateAtom)}), 
+    set: ({set, get}, pending_invitations) => { 
+        let userStateCopy = {...get(userStateAtom)}
+        userStateCopy.pending_invitations = pending_invitations; 
+        set(userStateAtom, userStateCopy)
+    }, 
 });
