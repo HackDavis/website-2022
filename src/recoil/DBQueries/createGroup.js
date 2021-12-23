62469e2bd5e84649af022f0ef3d7187a9462623e
Jalastin <justin_rusit@yahoo.com>
2021-12-23 14:53:54 -0800

    allowed user input for createGroup

--- src/recoil/DBQueries/createGroup.js
+++ src/recoil/DBQueries/createGroup.js
@@ -1,11 +1,11 @@
 import { doc, getDoc, setDoc, updateDoc, addDoc, collection } from "firebase/firestore";
 import { dbConfig } from "../../db/dbConfig.js";
 
-export async function createGroup(email, newGroupID, user_id) {
+export async function createGroup(email, newGroupID, user_id, newGroupDesc) {
     // first, create a group locally
     var newGroup = {
         contact_email: email,
-        description: "New Group Test Again!",
+        description: newGroupDesc,
         group_id: newGroupID,
         members: [user_id],
         pending_members: [],
@@ -17,8 +17,8 @@ export async function createGroup(email, newGroupID, user_id) {
     await addDoc(collection(dbConfig, "2022-groups"), newGroup);
 
     // update the current user's group on firebase
-    const docRef2 = doc(dbConfig, "2022-users", user_id);
-    await updateDoc(docRef2, {
+    const docRef = doc(dbConfig, "2022-users", user_id);
+    await updateDoc(docRef, {
         group_id: newGroupID,
         pending_groups: [],
     });
--- src/recoil/testButtons/CreateGroupButton.js
+++ src/recoil/testButtons/CreateGroupButton.js
@@ -13,9 +13,10 @@ function CreateGroupButton({}) {
     const setUserPendingInvitations = useSetRecoilState(SetUserPendingInvitations);
 
     async function createGroupClick() {
-        const newGroupID = "rgt test 2";
+        const newGroupID = document.getElementById("inputGroupID").value;
+        const newGroupDesc = document.getElementById("inputGroupDesc").value;
 
-        const newGroup = await createGroup(user.email, newGroupID, user.user_id);
+        const newGroup = await createGroup(user.email, newGroupID, user.user_id, newGroupDesc);
         // setRSVP for front-end Recoil atom
         setGroup(newGroup);
         setUserGroupID(newGroupID);
@@ -25,6 +26,14 @@ function CreateGroupButton({}) {
     
     return (
         <div>
+            <div>
+                Group ID: <br />
+                <input type="text" id="inputGroupID"></input>
+            </div>
+            <div>
+                Group Description: <br />
+                <input type="text" id="inputGroupDesc"></input>
+            </div>
             <button onClick={createGroupClick}>createGroup</button>
             {user ? <h1>{user.group_id}</h1> : <h1> user group not set</h1>}
         </div>
