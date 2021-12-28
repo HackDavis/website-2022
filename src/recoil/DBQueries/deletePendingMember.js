417d7e038fb21bf03db55c83f22eecee8e7abc44
trishnasharma <trishnasharma@users.noreply.github.com>
2021-12-27 20:14:36 -0800

    fixed denyGroupRequest

--- src/recoil/DBQueries/deletePendingMember.js
+++ src/recoil/DBQueries/deletePendingMember.js
@@ -1,4 +1,4 @@
-import { doc, updateDoc} from "firebase/firestore";
+import { doc, getDoc, updateDoc} from "firebase/firestore";
 import { dbConfig } from "../../db/dbConfig.js";
 
 export async function deletePendingMember(group_id, new_pending_members) {
@@ -7,7 +7,6 @@ export async function deletePendingMember(group_id, new_pending_members) {
         await updateDoc(docRef, {
             pending_members: new_pending_members,
         });
-        console.log("deletePendingMember success");
     } catch(e) {
         console.log(`deletePendingMember error ${e}`);
     }
--- src/recoil/testButtons/DenyGroupRequestButton.js
+++ src/recoil/testButtons/DenyGroupRequestButton.js
@@ -5,6 +5,7 @@ import { groupStateAtom } from "../atoms/groupAtom";
 import { DeletePendingGroup } from "../selectors/deletePendingGroup";
 import {DeletePendingMember} from "../selectors/deletePendingMember";
 import { denyGroupRequest } from "../DBQueries/denyGroupRequest.js"; 
+import { getGroup } from "../DBQueries/getGroup.js";
 
 function DenyGroupRequestButton() {
     const [user, setUser] = useRecoilState(userStateAtom);
@@ -13,26 +14,25 @@ function DenyGroupRequestButton() {
     const setDeletePendingMember = useSetRecoilState(DeletePendingMember);
 
     async function denyGroupRequestClick() { 
+        console.log(await getGroup(group.group_id));
         // local updates 
         let new_pending_members = {...group.pending_members}; // must spread because this is an object of arrays - 
                                                               // otherwise you do a shallow copy of nested objects 
         delete new_pending_members[user.user_id]; 
         let new_pending_groups = [...user.pending_groups]; // done for the same reason as above (we don't want a read-only copy) 
-        console.log("Old:", new_pending_groups); 
 
         // splice requires the index of the group_id you are searching for
         // so try doing group.indexOf(group_id) ?
-        new_pending_groups.splice(0, group.group_id); // TO-DO: This seemingly does nothing 
+        let groupIndex = new_pending_groups.indexOf(group.group_id);
+        new_pending_groups.splice(groupIndex, 1); 
         
-        console.log("New:", new_pending_groups); 
-        // TO-DO: Neither of the two sub-functions this one calls work. One returns "success" but does nothing 
-        // And the other returns net::ERR_FAILED with a 200 code (which should normally indicate success)? 
+        console.log("new pending_groups and pending_members done"); 
         await denyGroupRequest(user.user_id, group.group_id, new_pending_members, new_pending_groups); 
 
         // front-end Recoil atom update 
-        // TO-DO: These both don't work - React can't seem to find the function for some reason. 
         setDeletePendingMember(new_pending_members); 
         setDeletePendingGroup(new_pending_groups); 
+        console.log(await getGroup(group.group_id));
     }; 
     
     return (
