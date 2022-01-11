62af90fe057b0e4e2c9dd6ec224617e7f11de359
ecni19 <ericni2000@gmail.com>
2022-01-11 13:35:13 -0800

    Made nav items clickable, fixed contact section mobile layout

--- src/front-end/components/section_desktop_navbar.js
+++ src/front-end/components/section_desktop_navbar.js
@@ -1,20 +1,17 @@
 import React, { useState } from 'react';
 import styles from "../css/section_desktop_navbar.module.scss";
-import Hamburger from "../images/HamburgerMenu.svg";
 
 const Section_Desktop_Navbar = () => {
   return (
-    <div>
-      <div className={styles.container}>
-        <div className={`${styles.circle}`}/>
-        <div className={`${styles.buttons}`}>
-            <p className={styles.button}>About us</p>
-            <p className={styles.button}>Info</p>
-            <p className={styles.button}>FAQ</p>
-            <p className={styles.button}>Sponsors</p>
-            <p className={styles.button}>Profile</p>
+    <div className={styles.container}>
+      <div className={styles.circle} />
+        <div className={styles.buttons}>
+          <a href=""><p className={styles.button}>About us</p></a>
+          <a href=""><p className={styles.button}>Info</p></a>
+          <a href=""><p className={styles.button}>FAQ</p></a>
+          <a href=""><p className={styles.button}>Sponsors</p></a>
+          <a href=""><p className={styles.button}>Profile</p></a>
         </div>
-      </div>
     </div>
   );
 }
