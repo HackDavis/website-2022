567261a4d7c9ebdb479cd40e4b380c9c470c1354
Alec Atienza <alec@null.net>
2022-01-23 13:59:49 -0800

    slight animation changes

--- src/front-end/components/landingPage/mainSection/CowHeart.js
+++ src/front-end/components/landingPage/mainSection/CowHeart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../../css/landingPage/mainSection/CowHeart.module.scss";
 import base_image from '../../../images/main_section/cow_heart/base_image.svg';
 import code_box from '../../../images/main_section/cow_heart/code_box.svg';
@@ -8,12 +8,13 @@ import hand_up from '../../../images/main_section/cow_heart/hand_up.svg';
 import hand_down from '../../../images/main_section/cow_heart/hand_down.svg';
 
 export function CowHeart() {
+  const [animateHand, setAnimateHand] = useState(false);
 
   return (
     <>
       <section className={styles.container}>
-        <img className={styles.base_image} src={base_image} alt="cow image" />
-        <img className={styles.hand_up} src={hand_up} alt="cow hand" />
+        <img className={styles.base_image} src={base_image} alt="cow image" onMouseMove={() => setAnimateHand(true)} />
+        <img className={animateHand ? `${styles.hand_up} ${styles.hand_animate}` : `${styles.hand_up}`} onAnimationEnd={() => setAnimateHand(false)} src={hand_up} alt="cow hand" />
         <img className={styles.lightbulb} src={lightbulb} alt="lightbulb" />
         <img className={styles.code_box} src={code_box} alt="code box" />
         <img className={styles.heart} src={heart} alt="heart" />
