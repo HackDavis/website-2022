73d6f9c284165a39c41ffd1ea79b795fbd2085c0
ecni19 <ericni2000@gmail.com>
2022-01-25 16:51:33 -0800

    Added confetti to adventure section

--- src/front-end/components/section_adventure.js
+++ src/front-end/components/section_adventure.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "../css/section_adventure.module.scss";
 import AdventureDesktop from "../images/AdventureDesktop.svg";
 import AdventureMobile from "../images/AdventureMobile.svg";
+import Lottie from 'react-lottie';
+import animationData from '../lotties/confetti.json';
 
 const Section_Adventure = () => {
-    return (
-      <div className={styles.container}>
-        <p className={styles.headerText}>START YOUR ADVENTURE TODAY.</p>
-        <button className={styles.button}>PRE-REGISTER</button>
-        <img src={AdventureDesktop} className={styles.adventuredesktop} />
-        <img src={AdventureMobile} className={styles.adventuremobile} />
-      </div>
-    );
+  // const[isPaused, setIsPaused] = useState(true);
+  const defaultOptions = {
+    // loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  }
+  return (
+    <div className={styles.container}>
+      {/* <div onMouseEnter={() => setIsPaused(!isPaused)}> */}
+      <Lottie
+        style={{
+          "z-index": "998",
+          position: "absolute",
+          cursor:"default",
+        }}
+        className={styles.confetti}
+        options={defaultOptions}
+        isClickToPauseDisabled={true}
+        // isPaused={isPaused}
+      />
+      {/* </div> */}
+      <p className={styles.headerText}>START YOUR ADVENTURE TODAY.</p>
+      <button className={styles.button}>PRE-REGISTER</button>
+      <img src={AdventureDesktop} className={styles.adventuredesktop} />
+      <img src={AdventureMobile} className={styles.adventuremobile} />
+    </div>
+  );
 }
 
 export default Section_Adventure;
