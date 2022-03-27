9e68d0ee0e12c5b803680f55242ee19048cfb563
Alex Long <alelong@ucdavis.edu>
2022-03-26 18:34:58 -0700

    added images for 404 and 401 page

--- src/front-end/pages/PageNotFound.js
+++ src/front-end/pages/PageNotFound.js
@@ -1,6 +1,7 @@
 import {React} from 'react';
 import styles from "front-end/css/errorPage.module.scss";
 import {Link} from "react-router-dom";
+import errorCode from "front-end/images/errorPages/404.svg";
 
 export function PageNotFound() {
 
@@ -8,7 +9,7 @@ export function PageNotFound() {
     <div className={styles.container}>
       <h2>Uh Oh! This page does not exist...</h2>
       <h3>Please head back to the <Link to="/">homepage</Link>.</h3>
-      <img></img>
+      <img src={errorCode} className={styles.errorCode}></img>
     </div>
   );
 }
