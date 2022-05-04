04a42ff338c3e7775fc74e6070a60177eb9bc735
Jalastin <57930712+Jalastin@users.noreply.github.com>
2022-05-04 16:52:42 -0700

    separated finance + sponsorship

--- src/back-end/PrizeCardInfo.js
+++ src/back-end/PrizeCardInfo.js
@@ -12,16 +12,16 @@ export const PrizeCardInfo = [
 		color: "green"
 	},
 	{
-		title: "Marketing",
-		prize: "We’re looking for great communicators who are passionate about marketing and looking to gain experience in advertising for a distinguished hackathon. You’ll get to handle all public relations and social media accounts for our hackathon, as well as lead the creation and implementation of marketing strategies, receiving real-time feedback on your strategies. Working in our marketing team allows you to handle tasks ranging from authoring Medium articles and handing outreach with campus organizations and other universities, to updating our social media accounts and planning the entire marketing process to drive more turnout to our event and workshops than ever before. ",
-	description: "",
-	color: "green"
+		title: "Sponsorship",
+		prize: "We’re looking for great communicators who want experience in networking and making pitches to join the sponsorship team. In the last few years, we’ve successfully raised nearly $500,000 and partnered with school departments and companies like Google, Amazon, Intel, and IBM. You’ll be the face of HackDavis and serve as the point of contact for our sponsorship deals, and will be able to increase your professional network by forming relationships with campus leadership, recruiters, developer advocates, and other company representatives.",
+		description: "",
+		color: "green"
 	},
 	{
-		title: "Finance & Sponsorship",
-		prize: "We’re looking for detail-oriented people who are great communicators and want hands-on experience in raising and managing finances for a large-scale event. Each year, this team raises and manages more than $150,000 by partnering with school departments and companies like Google, Twilio, and Amazon. You will serve as the point of contact for our sponsorship deals, and will be able to increase your professional network by forming relationships with campus leadership, recruiters, developer advocates, and other company representatives. You’ll also get hands-on experience with project management, accounting, grant application, and invoicing.",
-	description: "",
-	color: "green"
+		title: "Design",
+		prize: "We’re looking for designers who want to gain hands-on skills in graphic design, digital marketing, and creating clean web experiences. You will work cross-functionally in a multi-project environment and gain valuable experience supporting social media strategy, building a brand identity, and synthesizing the creative vision for HackDavis. Your concepts will go live through our website, social media, and branded swag.",
+    description: "",
+    color: "yellow"
 	},
 	{
 		title: "External Affairs",
@@ -30,9 +30,15 @@ export const PrizeCardInfo = [
 		color: "yellow"
 	},
 	{
-		title: "Design",
-		prize: "We’re looking for designers who want to gain hands-on skills in graphic design, digital marketing, and creating clean web experiences. You will work cross-functionally in a multi-project environment and gain valuable experience supporting social media strategy, building a brand identity, and synthesizing the creative vision for HackDavis. Your concepts will go live through our website, social media, and branded swag.",
-    description: "",
-    color: "yellow"
+		title: "Finance",
+		prize: "We’re looking for detail-oriented people who want to obtain hands-on experience managing finances for a large-scale event. Working closely with the sponsorship team, we’ve carefully managed nearly $500,000 in sponsor funds and grants in the past few years. You’ll get hands-on experience with nonprofit management, accounting, grant application, invoicing, and serving as the point of contact with sponsors and vendors for payment logistics.",
+	description: "",
+	color: "green"
+	},
+	{
+		title: "Marketing",
+		prize: "We’re looking for great communicators who are passionate about marketing and looking to gain experience in advertising for a distinguished hackathon. You’ll get to handle all public relations and social media accounts for our hackathon, as well as lead the creation and implementation of marketing strategies, receiving real-time feedback on your strategies. Working in our marketing team allows you to handle tasks ranging from authoring Medium articles and handing outreach with campus organizations and other universities, to updating our social media accounts and planning the entire marketing process to drive more turnout to our event and workshops than ever before. ",
+	description: "",
+	color: "green"
 	}
 ];
