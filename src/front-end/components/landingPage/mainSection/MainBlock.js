fce750e5eb071f43c7c884e3af5ed5218fa329ff
trishnasharma <70460449+trishnasharma@users.noreply.github.com>
2022-07-18 13:04:12 -0700

    comment out correctly

--- src/front-end/components/landingPage/mainSection/MainBlock.js
+++ src/front-end/components/landingPage/mainSection/MainBlock.js
@@ -16,10 +16,10 @@ export function MainBlock() {
         <div className={`${styles.info_flex} ${styles.location_container} `}>
           {/* <img src={location_icon} className={styles.location_icon} /> */}
           {/* <p><a target="_blank" href="https://hackdavis.typeform.com/director2023" >Want to be a director for HackDavis 2023? Now's your chance!</a></p> */}
-//           <p>Apply to join the director team by <strong>May 15!</strong> :)</p>
+           {/* <p>Apply to join the director team by <strong>May 15!</strong> :)</p> */}
         </div>
         <div>
-//           <button className={styles.hover_prereg} onClick={() => window.open('https://hackdavis.typeform.com/director2023')}>APPLY</button>
+         {/*  <button className={styles.hover_prereg} onClick={() => window.open('https://hackdavis.typeform.com/director2023')}>APPLY</button> */}
           <button className={styles.hover_sponsor} onClick={() => window.open('mailto:team@hackdavis.io')}>SPONSOR 2023</button>
         </div>
         {/* <p>
